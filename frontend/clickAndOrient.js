94ea7b7898fb8ff59754120872c60a82aa9a7bc4
Tim Adamson <tda5@uw.edu>
2017-09-21 09:56:32 -0700

    brought in stashed svg change

--- frontend/clickAndOrient.js
+++ frontend/clickAndOrient.js
@@ -5,6 +5,18 @@
 var downX;
 var downY;
 
+function addLine(x1,y1, x2, y2, camera_name){
+    var svg = document.querySelector("#" + camera_name + "  svg");
+    var line = document.createElement("line");
+    line.setAttribute('x1', x1);
+    line.setAttribute('y1', y1);
+    line.setAttribute('x2', x2);
+    line.setAttribute('y2', y2);
+    line.setAttribute('stroke', "red");
+    line.setAttribute('stroke-width', "2");
+    svg.appendChild(line);
+}
+
 function init() {
     var arm_div = document.querySelectorAll('.js_arm_div');
     this.app = new App();
@@ -37,6 +49,7 @@ function init() {
                     var angle = Math.atan2(e.offsetY - downY, e.offsetX - downX);
                     console.log(angle);
                     self.app.arm.moveAndOrient(e.offsetX, e.offsetY, angle, elementId);
+                    addLine(downX, downY, e.offsetX, e.offsetY, elementId);
                     console.log("The angle we got is " + angle);
                 }
             };
