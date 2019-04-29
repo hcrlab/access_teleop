a27e7b437611a96559fafe80f058d79ea19e08fe
Tim Adamson <tda5@uw.edu>
2019-04-29 11:36:00 -0400

    added comments to arm.js

--- frontend/common_js/arm.js
+++ frontend/common_js/arm.js
@@ -30,6 +30,10 @@ Arm = function (ros) {
     });
 
     // A function for publishing to /access_teleop/delta
+    // Moves the end effector some delta x and delta y, which is the difference between the current x and y and the desired
+    // x and y. The cameraName is needed so that the backend can determine which world coordinates the x and y
+    // correlate with. The function simply creates a ROS message with the x, y, and cameraName passed in and then sends
+    // that message to the ROS backend.
     this.moveArmByDelta = function (deltaX, deltaY, cameraName) {
         if(cameraName) {
             var deltaPX = new ROSLIB.Message({
@@ -42,6 +46,10 @@ Arm = function (ros) {
     };
 
     // A function for publishing to /access_teleop/absolute
+    // Moves the end effector to an absolute point x, y. Is agnostic to where the end effector is currently at. The
+    // cameraName is needed so that the backend can determine which world coordinates the x and y correlate with. The
+    // function simply creates a ROS message with the x, y, and cameraName passed in and then sends that message to
+    // the ROS backend.
     this.moveArmByAbsolute = function (absX, absY, cameraName) {
         if(cameraName) {
             var absolutePX = new ROSLIB.Message({
@@ -53,6 +61,8 @@ Arm = function (ros) {
         }
     };
 
+    // Moves the end effector to an absolute point x, y, and changes the orientation of the end effector. All movement
+    // and orientation change is in a 2D plane that is orthogonal to the camera specified by cameraName.
     this.moveAndOrient = function (absX, absY, theta, cameraName) {
         var absoluteAndTheta = new ROSLIB.Message({
             camera_name: cameraName,
@@ -63,6 +73,8 @@ Arm = function (ros) {
         moveAndOrient.publish(absoluteAndTheta);
     };
 
+    // Changes the orientation of the end effector to be theta. The orientation change is in a 2D plane that is
+    // orthogonal to the camera specified by cameraName.
     this.orientByTheta = function(theta, cameraName){
         var theta = new ROSLIB.Message({
             camera_name: cameraName,
